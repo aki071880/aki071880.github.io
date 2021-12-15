dbb83251199bac5b3850cc66b3f7b25e2c01a2cb
aki071880 <96014119+aki071880@users.noreply.github.com>
2021-12-15 11:56:43 +0900

    Update rykou.js

--- rykou.js
+++ rykou.js
@@ -1,6 +1,6 @@
 $(function(){
     $('.btn').click(function(){
-        var $plan = $(this).find('p')
+        var $plan = $(this).find('.plan-contents')
         if($plan.hasClass('open')){
             $plan.removeClass('open');
             $plan.slideUp();
