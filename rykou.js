81505f0bb455dd75790fb21cce69c7cc19920189
aki071880 <96014119+aki071880@users.noreply.github.com>
2021-12-18 22:08:09 +0900

    Update rykou.js

--- rykou.js
+++ rykou.js
@@ -10,4 +10,19 @@ $(function(){
             $(this).find('p').slideUp();
         }
     });
-});
+    var interval =3000; 
+    var fade_speed = 1000;
+    $(".fade-img-box img").hide();
+    $(".fade-img-box img:first").addClass("active").show();
+
+        var changeImage = function(){
+		var $active = $(".fade-img-box img.active");
+		var $next = $active.next("img").length?$active.next("img"):$(".fade-img-box img:first");
+   
+		$active.fadeOut(fade_speed).removeClass("active");
+		$next.fadeIn(fade_speed).addClass("active");
+    }
+
+
+    setInterval(changeImage,interval);
+}());
